a508b0ee4af6f1b8dcd9b8e5f754f5223f656211
hayanj <hayaaljuraysi@gmail.com>
2022-02-20 11:33:57 +0300

    added admin view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # GET /users or /users.json
   def index
@@ -47,6 +47,9 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
 
   private
@@ -61,8 +64,8 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
-        flash[:alert] = "You can only edit your own account"
+      if current_user != @user && !current_user.admin?
+        flash[:alert] = "You can only edit or delete your own account"
         redirect_to @user
       end
     end
